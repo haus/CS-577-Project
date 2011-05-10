149b9fa626a80ece66bfb307881f408fb1f4c51f
Matthaus Litteken <mlitteken@gmail.com>
2011-05-09 17:52:45 -0700

    integration.rb needs to make newlines so it doesn't mess up the cursor in terminal.

--- sandbox/integration.rb
+++ sandbox/integration.rb
@@ -9,6 +9,7 @@ include_class 'AstShim'
 source = ARGV[0] || 'examples/if.fab'
 program = AstShim::giveMeAST(source)
 print program.to_pretty(0)
+print "\n"
 
 class Foo
   include Ast::TypeExpVisitor
