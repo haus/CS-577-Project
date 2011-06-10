7c6b8d6d5053e346ffc44a0985f3279b19a20c02
Matthaus Litteken <mlitteken@gmail.com>
2011-06-10 16:22:34 -0700

    Added binary compilation to the script. Magic.

--- bm/genAll.sh
+++ bm/genAll.sh
@@ -11,10 +11,12 @@ do
 	fablCMD="$fabl $F none"
 	$fablCMD 2> $filename.ll
 	llc -o $filename.s $filename.ll
+	gcc -o $filename.bin $filename.s
 done
 
 mv bm/fab/*.ll bm/llvm-ir-noOpt
 mv bm/fab/*.s bm/llvm-assembly-noOpt
+mv bm/fab/*.bin bm/llvm-binary-noOpt
 
 for F in `ls bm/fab/*.fab`;
 do 
@@ -24,10 +26,12 @@ do
 	fablCMD="$fabl $F half"
 	$fablCMD 2> $filename.ll
 	llc -o $filename.s $filename.ll
+	gcc -o $filename.bin $filename.s
 done
 
 mv bm/fab/*.ll bm/llvm-ir-halfOpt
 mv bm/fab/*.s bm/llvm-assembly-halfOpt
+mv bm/fab/*.bin bm/llvm-binary-halfOpt
 
 for F in `ls bm/fab/*.fab`;
 do 
@@ -37,7 +41,9 @@ do
 	fablCMD="$fabl $F full"
 	$fablCMD 2> $filename.ll
 	llc -o $filename.s $filename.ll
+	gcc -o $filename.bin $filename.s
 done
 
 mv bm/fab/*.ll bm/llvm-ir-fullOpt
 mv bm/fab/*.s bm/llvm-assembly-fullOpt
+mv bm/fab/*.bin bm/llvm-binary-fullOpt
