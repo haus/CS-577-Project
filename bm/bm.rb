ff7fe47c8ceba4f5665a15c9085153fd26db9579
Matthaus Litteken <mlitteken@gmail.com>
2011-06-10 23:04:57 -0700

    More binary files for benchmarking. Changed benchmark script to run interpreter a factor of 10 less times. Made deliverables directory.

--- bm/bm.rb
+++ bm/bm.rb
@@ -24,6 +24,7 @@ binDirs.each do |basedir|
   puts "\n"
 end
 
+n = n / 10
 # Time the llvm-interpreter...
 irDirs.each do |basedir|
   puts "#{basedir} - #{n} times"
@@ -43,9 +44,26 @@ puts "Reference Interpreter"
 puts "====================="
 Benchmark.bm(20) do |x|
   Dir.new("fab").entries.each do |file|
-    if File.file?("#{basedir}/#{file}") and file != "primes.fab"
+    if File.file?("fab/#{file}") and file != "primes.fab"
       x.report("#{file}") { for i in 1..n; `java -classpath ref-interpreter/frontend.jar:ref-interpreter/interp.jar "fab/#{file}"`; end}
     end
   end
+end
+puts "\n"
+
+# Primes is slower, so do it less...say 5 times total...
+# Time the noop on the ref-interpreter...
+n = 5
+puts "Primes Results - #{n} times"
+puts "====================="
+Benchmark.bm(20) do |x|
+  x.report("binary-noOpt") { for i in 1..n; `llvm-binary-noOpt/primes.bin`; end}
+  x.report("binary-halfOpt") { for i in 1..n; `llvm-binary-halfOpt/primes.bin`; end}
+  x.report("binary-fullOpt") { for i in 1..n; `llvm-binary-fullOpt/primes.bin`; end}
+  x.report("ir-noOpt") { for i in 1..n; `lli llvm-ir-noOpt/primes.ll`; end}
+  x.report("ir-halfOpt") { for i in 1..n; `lli llvm-ir-halfOpt/primes.ll`; end}
+  x.report("ir-fullOpt") { for i in 1..n; `lli llvm-ir-fullOpt/primes.ll`; end}
+  x.report("ref-binary") { for i in 1..n; `ref-binary/primes.bin`; end}
+  x.report("ref-interpreter") { for i in 1..n; `java -classpath ref-interpreter/frontend.jar:ref-interpreter/interp.jar "fab/primes.fab"`; end}
 end
 puts "\n"
